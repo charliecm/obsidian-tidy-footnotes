dc278d3717432ac83af2d04a7246753acc8cad6e
Charlie Chao <charliechao@outlook.com>
2021-04-17 12:57:29 -0700

    Update install script.

--- install-built.sh
+++ install-built.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 VAULT="$1"
-TARGET="$VAULT/.obsidian/plugins/dataview/"
+TARGET="$VAULT/.obsidian/plugins/open-with-optimage"
 mkdir -p $TARGET
-cp -f build/main.js styles.css manifest.json "$TARGET"
-echo Installed plugin files to "$TARGET"
+rm -f "$TARGET/main.js" "$TARGET/styles.css" "$TARGET/manifest.json"
+if [[ $2 == "-d" ]]; then
+  # Create symbolic links
+  ln -s $(pwd)/main.js "$TARGET"
+  ln -s $(pwd)/styles.css "$TARGET"
+  ln -s $(pwd)/manifest.json "$TARGET"
+  echo Linked plugin files to "$TARGET"
+else
+  # Copy built files once
+  cp -f main.js styles.css manifest.json "$TARGET"
+  echo Installed plugin files to "$TARGET"
+fi
