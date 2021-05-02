5f5feb6ba1258017c75353b4aedbdc194f791526
Charlie Chao <charliechao@outlook.com>
2021-05-02 13:55:28 -0700

    Simplify jest config.

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,6 @@ module.exports = {
     "**/?(*.)+(spec|test).+(ts)"
   ],
   "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-  globals: {
-    'ts-jest': {
-      diagnostics: {
-        ignoreCodes: [151001]
-      }
-    }
+    "^.+\\.(ts)$": "ts-jest"
   }
 }
